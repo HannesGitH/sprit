5613189cd6bf37ef67ce59e11c6bf70ca9e8a54d
HannesH <33062605+HannesGitH@users.noreply.github.com>
2023-01-08 12:52:41 +0100

    i dont get it now ts-dotenv doenst work

--- src/index.test.ts
+++ src/index.test.ts
@@ -11,10 +11,14 @@ import fs from 'fs';
 describe('.env loading', () => {
 	it('has a .env', () => {
 		expect(fs.existsSync('.env')).toBe(true); //succeeds
-		console.log(fs.readFileSync('.env')); //api_key=123456
+		console.log(fs.readFileSync('.env', 'utf-8')); //api_key=123456
 	});
 	it('loads .env ts', () => {
 		const env = denv.load({ mapbox_api_key: String }); //already fails here if i .load({api_key : String})
 		expect(env.mapbox_api_key).toBeDefined(); //FAILS
+	});
+	it('loads .env', () => {
+		require('dotenv').config();
+		expect(process.env.mapbox_api_key).toBeDefined(); //succeeds
 	});
 });
