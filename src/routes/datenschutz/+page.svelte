d36a9611c1c7ea900f266600376e1c9cbfb21a11
HannesH <33062605+HannesGitH@users.noreply.github.com>
2023-01-11 22:14:28 +0100

    pesky route

---
+++ src/routes/datenschutz/+page.svelte
@@ -0,0 +1,23 @@
+<div>
+	<p>
+		Es werden von mir keine zum Betrieb nicht notwendigen Daten verwendet. Und nichts von mir
+		gespeichert.
+	</p>
+
+	<p>Die Website wird von <a href="https://vercel.com/legal/privacy-policy">Vercel</a> gehostet.</p>
+
+	<p>
+		Um Tankstellen in der Nähe zu finden, kann die Position des Nutzers abgefragt werden. Diese wird
+		für eine Umkreisabfrage an <a href="https://tankerkoenig.de/datenschutz.php">tankerkönig.de</a> weitergeleitet.
+	</p>
+
+	<p>
+		Als kartendiesntleister wird <a href="https://www.mapbox.com/legal/privacy/">mapbox</a> verwendet.
+	</p>
+</div>
+
+<style>
+	div {
+		padding: 4rem 1rem 1rem 1rem;
+	}
+</style>
